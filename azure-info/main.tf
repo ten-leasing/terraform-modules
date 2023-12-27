76f1dc34404cd74fbd7175acc8807c0b4c821802
Nathan Blair <me@nathanblair.rocks>
2023-12-27 12:07:01 -0500

    initial commit of azure info for output

---
+++ azure-info/main.tf
@@ -0,0 +1,29 @@
+data "azurerm_subscription" "current" {}
+
+data "azurerm_client_config" "current" {}
+
+data "azuread_directory_object" "current" {
+  object_id = data.azurerm_client_config.current.object_id
+}
+
+data "azuread_service_principal" "current" {
+  count     = data.azuread_directory_object.current.type == "ServicePrincipal" ? 1 : 0
+  object_id = data.azurerm_client_config.current.object_id
+}
+
+data "azuread_user" "current" {
+  count     = data.azuread_directory_object.current.type == "User" ? 1 : 0
+  object_id = data.azurerm_client_config.current.object_id
+}
+
+output "provisioner_user_name" {
+  value = (
+    data.azuread_directory_object.current.type == "User" ?
+    data.azuread_user.current.0.display_name :
+    data.azuread_service_principal.current.0.display_name
+  )
+}
+
+output "provisioner_object_id" {
+  value = data.azurerm_client_config.current.object_id
+}
