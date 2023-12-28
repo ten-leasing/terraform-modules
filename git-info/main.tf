910567d62005a530e7a7be1924bb9bb6212ed71e
Nathan Blair <me@nathanblair.rocks>
2023-12-27 19:56:45 -0500

    fix capture group

--- git-info/main.tf
+++ git-info/main.tf
@@ -1,13 +1,8 @@
 locals {
   git_config_content = file(".git/config")
-  remotes            = regex("\\[remote \"(.*)\"\\]\\n.*url.*=.*(https.*)", local.git_config_content)
-  all_remote         = regexall("\\[remote \"(.*)\"\\]\\n.*url.*=.*(https.*)", local.git_config_content)
+  remotes            = regexall("\\[remote (?:\"(?P<name>.*)\"\\]\\n.*url.*=.*(?P<url>https.*))", local.git_config_content)
 }
 
 output "git_remotes" {
-  value = local.remotes
-}
-
-output "git_all_remotes" {
   value = local.remotes
 }
