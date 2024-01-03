5b8755f5fd6c2659b1c877081cb0bb59b1c069d7
Nathan Blair <me@nathanblair.rocks>
2024-01-03 04:20:25 -0500

    correct resource group name

--- reporting/variables.tf
+++ reporting/variables.tf
@@ -6,7 +6,7 @@ variable "resource_group_name" {
 }
 
 output "resource_group_name" {
-  value = azurerm_public_ip.gateway.resource_group_name
+  value = azurerm_virtual_network.main.resource_group_name
 }
 
 variable "location" {
