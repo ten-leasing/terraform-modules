204e773a2fd6faced9df122999add8c7590abdc0
Nathan Blair <me@nathanblair.rocks>
2024-01-03 14:42:11 -0500

    don't need a list of string for managed instance vnet address space

--- data/network.tf
+++ data/network.tf
@@ -2,7 +2,7 @@ locals {
 }
 
 variable "vnet_address_space" {
-  type = list(string)
+  type = string
 }
 
 variable "hub_vnet_resource_group_name" {
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "managed_instance" {
   tags                = merge(var.tags, {})
 
   name          = "${var.resource_name_prefix}-mi-vnet"
-  address_space = var.vnet_address_space
+  address_space = [var.vnet_address_space]
 }
 
 output "vnet_resource_group_name" {
