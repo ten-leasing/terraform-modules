7e85347a21bb7f9caa3921097ec8dc8d303b81ae
Nathan Blair <me@nathanblair.rocks>
2024-01-03 19:57:52 -0500

    fix type

--- images/packer.tf
+++ images/packer.tf
@@ -6,6 +6,6 @@ resource "azurerm_subnet" "packer" {
   resource_group_name  = azurerm_virtual_network.main.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
 
-  address_prefixes = local.packer_subnet_address_prefixes
+  address_prefixes = [local.packer_subnet_address_prefixes]
   name             = "packer"
 }
