19448dc56a5e40277434ba150c86e845d011b65d
Nathan Blair <me@nathanblair.rocks>
2024-01-04 04:43:54 -0500

    parameterize enabling reporting peerings

--- reporting/peerings.tf
+++ reporting/peerings.tf
@@ -3,12 +3,18 @@ locals {
   reporting_vnet_name           = "vnet_prod_east_001"
 }
 
+variable "enable_peerings" {
+  type    = bool
+  default = true
+}
+
 data "azurerm_virtual_network" "old_reporting" {
   resource_group_name = local.reporting_resource_group_name
   name                = local.reporting_vnet_name
 }
 
 module "vnet_and_old_reporting_peering" {
+  count  = var.enable_peerings ? 1 : 0
   source = "git::https://star-leasing@dev.azure.com/star-leasing/Architecture/_git/terraform-library//azure-vnet-peerings?ref=default"
 
   vnet1_resource_group_name = var.vnet_resource_group_name
