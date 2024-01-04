e585a41d462b0bf5d419a185119f2c7583c07f7c
Nathan Blair <me@nathanblair.rocks>
2024-01-04 03:18:06 -0500

    adding variables for reporting peering

--- reporting/variables.tf
+++ reporting/variables.tf
@@ -5,8 +5,20 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "vnet_resource_group_name" {
+  type = string
+}
+
 variable "vnet_name" {
   type = string
+}
+
+variable "vnet_id" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
 }
 
 variable "subnet_address_prefixes" {
