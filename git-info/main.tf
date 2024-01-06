980a07e058e8c30c1033f29fcd157d21283f4f46
Nathan Blair <me@nathanblair.rocks>
2024-01-06 15:51:41 -0500

    flatten the result as well

--- git-info/main.tf
+++ git-info/main.tf
@@ -3,10 +3,10 @@ locals {
   git_remote_urls_set = var.git_remote_urls != []
   git_config_content  = fileexists(local.git_config_path) ? file("${local.git_config_path}") : ""
 
-  remotes = compact(
+  remotes = flatten(compact(
     compact(var.git_remote_urls),
     regexall("\\[remote (?:\"(?P<name>.*)\"\\]\\n.*url.*=.*(?P<url>https.*))", local.git_config_content),
-  )
+  ))
 }
 
 variable "git_remote_urls" {
