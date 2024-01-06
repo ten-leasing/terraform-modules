4f29778781974f525f6483e3a700c64dd0130323
Nathan Blair <me@nathanblair.rocks>
2024-01-06 15:38:35 -0500

    lets try this

--- git-info/main.tf
+++ git-info/main.tf
@@ -1,6 +1,16 @@
 locals {
-  git_config_content = file("${abspath(path.cwd)}/.git/config")
-  remotes            = regexall("\\[remote (?:\"(?P<name>.*)\"\\]\\n.*url.*=.*(?P<url>https.*))", local.git_config_content)
+  git_config_path     = "${path.cwd}/.git/config"
+  git_remote_urls_set = var.git_remote_urls != []
+  git_config_content  = fileexists(local.git_config_path) ? file("${local.git_config_path}") : ""
+
+  remotes = compact(
+    compact(var.git_remote_urls),
+    regexall("\\[remote (?:\"(?P<name>.*)\"\\]\\n.*url.*=.*(?P<url>https.*))", local.git_config_content),
+  )
+}
+
+variable "git_remote_urls" {
+  type = list(string)
 }
 
 output "remotes" {
