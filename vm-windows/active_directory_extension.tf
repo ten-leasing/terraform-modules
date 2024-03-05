aaad00d57997774db2a15c2248534dcfcb33c330
Nathan Blair <me@nathanblair.rocks>
2024-03-05 16:40:01 -0500

    permit setting the version of the AAD Login extension

--- vm-windows/active_directory_extension.tf
+++ vm-windows/active_directory_extension.tf
@@ -1,3 +1,7 @@
+variable "type_handler_version" {
+  type = string
+}
+
 resource "azurerm_virtual_machine_extension" "aad_login" {
   provider                   = azurerm.current
   name                       = "AADLogin"
@@ -5,6 +9,7 @@ resource "azurerm_virtual_machine_extension" "aad_login" {
   virtual_machine_id         = azurerm_windows_virtual_machine.self.id
   publisher                  = "Microsoft.Azure.ActiveDirectory"
   type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0"
-  auto_upgrade_minor_version = true
+  type_handler_version       = var.type_handler_version
+  automatic_upgrade_enabled  = false
+  auto_upgrade_minor_version = false
 }
