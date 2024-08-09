c76ea5c603a6306bf060a6d1321020248f580427
Nathan Blair <me@nathanblair.rocks>
2024-08-09 14:34:08 -0400

    build the directory extension back into the module

--- vm-windows/active_directory_extension.tf
+++ vm-windows/active_directory_extension.tf
@@ -18,3 +18,10 @@ resource "azurerm_virtual_machine_extension" "aad_login" {
   automatic_upgrade_enabled  = false
   auto_upgrade_minor_version = false
 }
+
+resource "azuread_directory_role" "directory_reader" { display_name = "Directory Readers" }
+
+resource "azuread_directory_role_assignment" "vm_entra_reader" {
+  principal_object_id = one(azurerm_windows_virtual_machine.self.identity).principal_id
+  role_id             = azuread_directory_role.directory_reader.template_id
+}
