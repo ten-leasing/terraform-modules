4507fe23f7652891a01ba77c01b3878c36a8b4cf
Nathan Blair <me@nathanblair.rocks>
2025-03-07 22:15:43 -0500

    add some cognitive services

---
+++ azure-naming/cognitive_services.tf
@@ -0,0 +1,30 @@
+locals {
+  open_ai_abbreviation = "oai"
+  open_ai = {
+    abbrev = local.open_ai_abbreviation
+    scope  = local.scopes.resource_group
+    parent = local.resource_group
+    name = format(
+      "%s-%s-%s",
+      local.open_ai_abbreviation,
+      var.ORG_KEY,
+      var.RESOURCE_NAME,
+    )
+  }
+
+  translator_abbreviation = "trsl"
+  translator = {
+    abbrev = local.translator_abbreviation
+    scope  = local.scopes.resource_group
+    parent = local.resource_group
+    name = format(
+      "%s-%s-%s",
+      local.translator_abbreviation,
+      var.ORG_KEY,
+      var.RESOURCE_NAME,
+    )
+  }
+}
+
+output "open_ai" { value = local.open_ai }
+output "translator" { value = local.translator }
---
+++ azure-naming/tests/cognitive_services.tftest.hcl
@@ -0,0 +1,23 @@
+run "open-ai" {
+  command = plan
+
+  assert {
+    condition = local.open_ai.name == format(
+      "%s-%s-%s",
+      local.open_ai.abbrev,
+      var.ORG_KEY,
+      var.RESOURCE_NAME,
+    )
+    error_message = "open-ai naming convention is incorrect"
+  }
+
+  assert {
+    condition = local.translator.name == format(
+      "%s-%s-%s",
+      local.translator.abbrev,
+      var.ORG_KEY,
+      var.RESOURCE_NAME,
+    )
+    error_message = "translator naming convention is incorrect"
+  }
+}
