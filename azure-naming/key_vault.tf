ae8b2c86e13bdf86eee3cb292271aab1f842982f
Nathan Blair <me@nathanblair.rocks>
2025-03-10 21:34:06 -0400

    add key vault secret

--- azure-naming/key_vault.tf
+++ azure-naming/key_vault.tf
@@ -12,6 +12,21 @@ locals {
       var.WORKSPACE == "default" ? "" : "-${var.WORKSPACE}"
     )
   }
+
+  key_vault_secret_abbreviation = "kvs"
+  key_vault_secret = {
+    abbrev = local.key_vault_secret_abbreviation
+    scope  = local.scopes.parent
+    parent = local.key_vault
+    name = format(
+      "%s-%s%s%s",
+      local.key_vault_secret_abbreviation,
+      var.ORG_KEY,
+      var.RESOURCE_NAME == "" ? "" : "-${var.RESOURCE_NAME}",
+      var.WORKSPACE == "default" ? "" : "-${var.WORKSPACE}"
+    )
+  }
 }
 
 output "key_vault" { value = local.key_vault }
+output "key_vault_secret" { value = local.key_vault_secret }
--- azure-naming/tests/key_vault.tftest.hcl
+++ azure-naming/tests/key_vault.tftest.hcl
@@ -11,4 +11,16 @@ run "key-vault" {
     )
     error_message = "key-vault naming convention is incorrect"
   }
+
+  assert {
+    condition = local.key_vault_secret.name == format(
+      "%s-%s%s%s",
+      local.key_vault_secret.abbrev,
+      var.ORG_KEY,
+      var.RESOURCE_NAME == "" ? "" : "-${var.RESOURCE_NAME}",
+      var.WORKSPACE == "default" ? "" : "-${var.WORKSPACE}"
+    )
+    error_message = "key-vault secret naming convention is incorrect"
+  }
+
 }
